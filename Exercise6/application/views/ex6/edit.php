4caa0c3ab59d67cdac832e21f77018f1b79ba943
Royce Martin <dvroyce08@gmail.com>
2016-12-22 08:46:01 +0800

    edited edit to match old database

--- Exercise6/application/views/ex6/edit.php
+++ Exercise6/application/views/ex6/edit.php
@@ -5,12 +5,32 @@
 <?php echo form_open('ex6/edit/'.$ex6_item['id']); ?>
     <table>
         <tr>
-            <td><label for="title">Title</label></td>
-            <td><input type="input" name="title" size="50" value="<?php echo $ex6_item['title'] ?>" /></td>
+            <td><label for="full_name">Full Name</label></td>
+            <td><input type="input" name="full_name" size="100" value="<?php echo $ex6_item['full_name'] ?>" /></td>
+        </tr>
+		<tr>
+            <td><label for="nick_name">Nickname</label></td>
+            <td><input type="input" name="nick_name" size="25" value="<?php echo $ex6_item['nick_name'] ?>" /></td>
+        </tr>
+		<tr>
+            <td><label for="email_address">Email Address</label></td>
+            <td><input type="input" name="email_address" size="45" value="<?php echo $ex6_item['email_address'] ?>" /></td>
+        </tr>
+		<tr>
+            <td><label for="home_address">Home Address</label></td>
+            <td><input type="input" name="home_address" size="100" value="<?php echo $ex6_item['home_address'] ?>" /></td>
+        </tr>
+		<tr>
+            <td><label for="gender">Gender</label></td>
+            <td><input type="input" name="gender" size="20" value="<?php echo $ex6_item['gender'] ?>" /></td>
+        </tr>
+		<tr>
+            <td><label for="cellphone_number">Cellphone Number</label></td>
+            <td><input type="input" name="cellphone_number" size="11" value="<?php echo $ex6_item['cellphone_number'] ?>" /></td>
         </tr>
         <tr>
-            <td><label for="text">Text</label></td>
-            <td><textarea name="text" rows="10" cols="40"><?php echo $ex6_item['text'] ?></textarea></td>
+            <td><label for="comments">Comments</label></td>
+            <td><textarea name="comments" rows="10" cols="40"><?php echo $ex6_item['comments'] ?></textarea></td>
         </tr>
         <tr>
             <td></td>
