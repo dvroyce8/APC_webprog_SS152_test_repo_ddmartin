3ddcbd4492f04b2bfa27528ff1c1eb04694c0816
Royce Martin <dvroyce08@gmail.com>
2016-12-22 08:05:00 +0800

    edit header.php(didnt se the commit for some reason

---
+++ Exercise6/application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+        <head>
+                <title>Exercise 6</title>
+        </head>
+        <body>
+ 
+                <h1>Simple CRUD</h1>
+                <p><a href="<?php echo site_url('ex6'); ?>">Home</a> | <a href="<?php echo site_url('ex6/create'); ?>">Add data</a></p>
