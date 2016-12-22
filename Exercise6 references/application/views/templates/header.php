379798b2d1d34877a1320a3d2672465b7966847c
Royce Martin <dvroyce08@gmail.com>
2016-12-22 11:25:53 +0800

    edited header.php

--- Exercise6 references/application/views/templates/header.php
+++ Exercise6 references/application/views/templates/header.php
@@ -1,8 +1,8 @@
 <html>
         <head>
-                <title>CodeIgniter Tutorial</title>
+                <title>Exercise 6</title>
         </head>
         <body>
  
                 <h1>Simple CRUD</h1>
-                <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+                <p><a href="<?php echo site_url('ex6'); ?>">Home</a> | <a href="<?php echo site_url('ex6/create'); ?>">Add data</a></p>
