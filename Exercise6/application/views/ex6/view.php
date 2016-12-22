82452814c9579f27ac79ed85fc658e234a3cf364
Royce Martin <dvroyce08@gmail.com>
2016-12-22 08:53:31 +0800

    edited view.php

--- Exercise6/application/views/ex6/view.php
+++ Exercise6/application/views/ex6/view.php
@@ -1,3 +1,8 @@
 <?php
-echo '<h2>'.$ex6_item['title'].'</h2>';
-echo $ex6_item['text'];
+echo '<h2>'.$ex6_item['full_name'].'</h2>';
+echo $ex6_item['nick_name'];
+echo $ex6_item['email_address'];
+echo $ex6_item['home_address'];
+echo $ex6_item['gender'];
+echo $ex6_item['cellphone_number'];
+echo $ex6_item['comments'];
