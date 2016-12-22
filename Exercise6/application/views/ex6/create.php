7e70962473d1d157740be735bfba1590a428692b
Royce Martin <dvroyce08@gmail.com>
2016-12-22 08:41:32 +0800

    Added old database values

--- Exercise6/application/views/ex6/create.php
+++ Exercise6/application/views/ex6/create.php
@@ -5,12 +5,31 @@
 <?php echo form_open('ex6/create'); ?>    
     <table>
         <tr>
-            <td><label for="title">Title</label></td>
-            <td><input type="input" name="title" size="50" /></td>
+            <td><label for="full_name">Full Name</label></td>
+            <td><input type="input" name="full_name" size="100" /></td>
+        </tr>
+		<tr>
+            <td><label for="nick_name">Nickname</label></td>
+            <td><input type="input" name="nick_name" size="25" /></td>
+        </tr>
+		<tr>
+            <td><label for="email_address">Email Address</label></td>
+            <td><input type="input" name="email_address" size="45" /></td>
+        </tr>
+		<tr>
+            <td><label for="home_address">Home Address</label></td>
+            <td><input type="input" name="home_address" size="100" /></td>
+        </tr>
+		<tr>
+            <td><label for="gender">Gender</label></td>
+            <td><input type="input" name="gender" size="20" /></td>
+        </tr><tr>
+            <td><label for="cellphone_number">Cellphone Number</label></td>
+            <td><input type="input" name="cellphone_number" size="11" /></td>
         </tr>
         <tr>
-            <td><label for="text">Text</label></td>
-            <td><textarea name="text" rows="10" cols="40"></textarea></td>
+            <td><label for="comments">Comments</label></td>
+            <td><textarea name="comments" rows="10" cols="40"></textarea></td>
         </tr>
         <tr>
             <td></td>
